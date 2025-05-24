457896f59725fcf7f5517a8a645c1090b1cc9ebb
Kishorevj97 <kishorevj97@gmail.com>
2025-05-24 17:43:16 +0530

    Prototype for Payroll

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATLkavIplt6QdpNBeo3CYGDmO_ItJn7us",
+  authDomain: "agilepayroll-ui.firebaseapp.com",
+  projectId: "agilepayroll-ui",
+  storageBucket: "agilepayroll-ui.firebasestorage.app",
+  messagingSenderId: "501248983614",
+  appId: "1:501248983614:web:bd8eacdc10de89bc1b876f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
